7c6516b06b47549166e0a1f08614287968ea5e8f
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2025-03-24 13:46:05 +0300

    Custom Table View Cell

---
+++ CafeTableApp/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  CafeTableApp
+//
+//  Created by Dmitry on 24.03.25.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+    
+    
+}
