a16ed0ef5b1d6fa82e8328722b7579f55b110b93
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2025-03-28 15:39:01 +0300

    after add Sorting

--- CafeTableApp/StorageManager.swift
+++ CafeTableApp/StorageManager.swift
@@ -17,4 +17,12 @@ class StorageManager {
         }
     }
     
+    static func deleteObject(_ place: Place) {
+        
+        try! realm.write {
+            realm.delete(place)
+        }
+    }
+    
+    
 }
