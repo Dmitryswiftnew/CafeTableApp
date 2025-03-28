bd86ffc89143764aa8cd006ac11523080faf80cb
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2025-03-28 11:38:35 +0300

    After installation DB Realm

---
+++ CafeTableApp/PlaceModel.swift
@@ -0,0 +1,48 @@
+//
+//  PlaceModel.swift
+//  CafeTableApp
+//
+//  Created by Dmitry on 24.03.25.
+//
+
+import RealmSwift
+
+
+
+class Place: Object {
+    
+    @objc dynamic var name = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var imageData: Data?
+    
+    
+   
+   let restaurantNames = [
+    "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+    "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+    "Speak Easy", "Morris Pub", "Вкусные истории",
+    "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+
+
+ func savePlaces() {
+        for place in restaurantNames {
+            
+            let image = UIImage(named: place)
+            guard let imageData = image?.pngData() else { return }
+            
+            let newPlace = Place()
+            newPlace.name = place
+            newPlace.location = "Minsk"
+            newPlace.type = "Resturant"
+            newPlace.imageData = imageData
+            
+            StorageManager.saveObject(newPlace)
+            
+        }
+    }
+    
+    
+    
+}
